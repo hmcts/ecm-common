799b7812dbe831f86b1cc639e6e205c9816831c8
gauravchawla <gaurav-chawla@hotmail.com>
2022-12-15 11:44:58 +0000

    revert

--- src/test/java/uk/gov/hmcts/ecm/common/model/servicebus/tasks/UpdateDataTaskTest.java
+++ src/test/java/uk/gov/hmcts/ecm/common/model/servicebus/tasks/UpdateDataTaskTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ecm.common.model.servicebus.tasks;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
