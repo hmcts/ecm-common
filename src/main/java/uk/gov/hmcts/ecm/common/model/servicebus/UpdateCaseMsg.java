9d584d9c4c38c9a39a1c748a5e8f849791361150
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-08 14:47:35 +0000

    ECM-1139 changes - first commit

--- src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/UpdateCaseMsg.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/UpdateCaseMsg.java
@@ -8,7 +8,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
-import uk.gov.hmcts.ecm.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.ecm.common.model.ccd.SubmitEvent;
 import uk.gov.hmcts.ecm.common.model.servicebus.datamodel.DataModelFactory;
 import uk.gov.hmcts.ecm.common.model.servicebus.tasks.*;
